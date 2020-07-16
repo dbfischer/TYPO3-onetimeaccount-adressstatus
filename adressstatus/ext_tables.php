5186fa3091ef9c067eec25ad689bf858506b4c4d
Dirk <dbfischer@web.de>
2020-07-16 16:24:53 +0200

    Update ext_tables.php
    bugfix

--- adressstatus/ext_tables.php
+++ adressstatus/ext_tables.php
@@ -1,26 +1,11 @@
 <?php
-if (!defined ('TYPO3_MODE')) {
-	die ('Access denied.');
-}
-$tempColumns = array (
-	'adressstatus' => array (
-		'exclude' => 0,
-		'label' => 'LLL:EXT:adressstatus/locallang_db.xml:fe_users.adressstatus',
-		'config' => array (
-			'type' => 'select',
-			'items' => array (
-				array('LLL:EXT:adressstatus/locallang_db.xml:fe_users.adressstatus.I.0', '0'),
-				array('LLL:EXT:adressstatus/locallang_db.xml:fe_users.adressstatus.I.1', '1'),
-			),
-			'size' => 1,
-			'maxitems' => 1,
-		)
-	),
+defined('TYPO3_MODE') || die('Access denied.');
+
+call_user_func(
+    function()
+    {
+
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('addressstatus', 'Configuration/TypoScript', 'One-time FE account addressstatus');
+
+    }
 );
-
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('fe_users',$tempColumns,1);
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('fe_users','adressstatus;;;;1-1-1');
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript/', 'adressstatus');
-?>
